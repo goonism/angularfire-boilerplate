9a4cef6e8d4b8b6f6df6595eb9aaac144323348f
Patrick Pistor <yogert96@gmail.com>
2017-11-10 20:59:33 -0500

    Add angularfire and update app.module

--- boilerplate/src/app/app.module.ts
+++ boilerplate/src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
 
 import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
+export const firebaseConfig = environment.firebaseConfig;
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+	AngularFireModule.initializeApp(environment.firebaseConfig),
+	AngularFireDatabaseModule,
+	AngularFireAuthModule,
+	AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
